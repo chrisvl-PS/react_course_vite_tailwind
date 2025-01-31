6dd354e0b15a4dab8e25eb79e5966f93f32357fc
chrisvl-PS <christian.vieyra@priceshoes.com>
2025-01-31 16:16:21 -0600

    react_course_vite_tailwind

---
+++ src/Context/index.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from 'react'
+
+export const ShoppingCartContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart · Increment quantity
+    const [count, setCount] = useState(0);
+
+    // Product Detail · Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Product Detail · Show product
+    const [productToShow, setProductToShow] = useState({});
+
+    // Shopping Cart - Add products  to cart
+    const [cartProducts, setCartProducts] = useState([]);
+
+    return (
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts,
+        }}>
+            {children}
+        </ShoppingCartContext.Provider>
+    );
+}
