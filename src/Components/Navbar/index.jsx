14bcf4a0d28c5c3402f85a268ae692293f5c203e
chrisvl-PS <christian.vieyra@priceshoes.com>
2025-02-11 11:24:28 -0600

    Filtrado por categorías con JavaScript

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/'
+                        onClick={() => context.setSearchByCategory()}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -27,6 +28,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/clothes'
+                        onClick={() => context.setSearchByCategory('clothes')}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -36,6 +38,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/electronics'
+                        onClick={() => context.setSearchByCategory('electronics')}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -45,6 +48,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/furnitures'
+                        onClick={() => context.setSearchByCategory('furnitures')}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -54,6 +58,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/toys'
+                        onClick={() => context.setSearchByCategory('toys')}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -63,6 +68,7 @@ const Navbar = () => {
                 <li>
                     <NavLink
                         to='/others'
+                        onClick={() => context.setSearchByCategory('others')}
                         className={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }>
@@ -105,7 +111,7 @@ const Navbar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
-                    <div>{context.count}</div>
+                    <div>{context.cartProducts.length}</div>
                 </li>
             </ul>
         </nav>
